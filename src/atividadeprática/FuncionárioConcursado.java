0b7e61515dd25a4016d37662cf827ec20ee00480
Edartista <f.vinicius.edart@gmail.com>
2020-06-27 14:14:56 -0300

    Atividade Prática realizada

--- src/atividadeprática/FuncionárioConcursado.java
+++ src/atividadeprática/FuncionárioConcursado.java
@@ -1,22 +1,27 @@
 package atividadeprática;
 public class FuncionárioConcursado extends Funcionário {
 
-    public FuncionárioConcursado(Integer código, Integer tipo, Double salárioBase, Integer tempo, Dependente dependentes) {
-        super(código, tipo, salárioBase, tempo, dependentes);
+    public FuncionárioConcursado(Integer código, Integer tipo, Double salárioBase, Integer tempo, Integer[] idades) {
+        super(código, tipo, salárioBase, tempo, idades);
     }
     
     @Override
     public double salário(){
-        
-        return (tempo * 200) + salárioBase + (númeroDependentesMenores * 100);    
+        double salario = (tempo * 200) + salárioBase;
+        for (int i=0; i<idades.length; i++){
+            if (idades[i] <21){
+                salario += 100;
+            }
+        }
+        return salario;
     }
     
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format("Funcionário com código %d, concursado (1), ", código));
-        sb.append(String.format("com salário-base de R$%.2f e ", salário()));
-        sb.append(tempo + " anos de contratação.");
+        sb.append(String.format("ID: %d %n", código));
+        sb.append(String.format("Ingressou na empresa há %d anos.%n", tempo));
+        sb.append(String.format("Remuneração mensal: R$%.2f. %n", salário()));
         return sb.toString();
         }
 }
